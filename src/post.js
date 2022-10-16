3971360c04169d35e4a3be9271bdb941e0bc5a1b
pnordboj <91888307+pnordboj@users.noreply.github.com>
2022-10-16 16:46:25 +0200

    Modified JsDoc index page
    Added return to home button
    
    Added all source pages to all page on JsDocs

--- src/post.js
+++ src/post.js
@@ -3,16 +3,18 @@
  */
 
 import "./js/viewPost.js";
-import "./js/Authenticate/login.js";
+
+const logoutButton = document.querySelector("#logout");
 
 function autoLogin() {
   if (isLoggedIn() === true) {
     const html = document.querySelector("#loggedInUser");
     html.innerHTML = `
-            <div class="shadow p-2 bg-body rounded" style="width: rem10;">
-                Welcome: ${localStorage.getItem("username")}
-            </div>
-            `;
+        <div class="shadow p-2 bg-body rounded" style="width: rem10;">
+            Welcome: ${localStorage.getItem("username")}
+        </div>
+        `;
+    logoutButton.classList.replace("d-none", "d-flex");
   }
 }
 
@@ -21,3 +23,8 @@ autoLogin();
 function isLoggedIn() {
   return Boolean(localStorage.getItem("access-token"));
 }
+
+logoutButton.onclick = function logout() {
+  localStorage.clear();
+  window.location.href = "/";
+};
